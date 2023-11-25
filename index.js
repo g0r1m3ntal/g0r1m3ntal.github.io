5a2fab118f4627d9e52d51374b5320c548780f98
g0r1m3ntal <151820769+g0r1m3ntal@users.noreply.github.com>
2023-11-25 20:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,3 +68,27 @@ updLstChr:function(){
 		this.write("|"); // else write it  
 }  
 }
+function replaceUrls(text) {var http = text.indexOf("http://");var space = text.indexOf(".me ", http);
+
+if (space != -1) {   
+	var url = text.slice(http, space-1);  
+	return text.replace(url, "<a href=\\","  + url + ","\\>" + url + "</a>");  
+}   
+  
+else {  
+	return text  
+}  
+}
+
+
+
+Typer.speed=3;Typer.file="g0r1m3ntal.txt"; // add your own name hereTyper.init();
+
+
+
+var timer = setInterval("t();", 30);function t() {Typer.addText({"keyCode": 123748});
+
+if (Typer.index > Typer.text.length) {  
+	clearInterval(timer);  
+}  
+}
